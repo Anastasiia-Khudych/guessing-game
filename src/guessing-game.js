5e18bfef9b3b37e464864185eb7e4f7b7229709c
Anastasiia-Khudych <anastasiiakhudych@gmail.com>
2022-08-16 19:33:48 +0300

    add guessing-game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
 
     guess() {
-
+        return Math.ceil((this.minNum + this.maxNum) / 2);
     }
 
     lower() {
-
+        return this.maxNum = Math.ceil((this.minNum + this.maxNum) / 2);
     }
 
     greater() {
-
+        return this.minNum = Math.ceil((this.minNum + this.maxNum) / 2);
     }
 }
 
